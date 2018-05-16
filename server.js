e51c26c8ee30bc999f2c0123a5333a30321e71f0
samho <samho@ugcv.com>
2018-05-16 14:57:19 +0800

    restructure

--- server.js
+++ server.js
@@ -16,38 +16,36 @@ const numCPUs = require('os').cpus().length;
 const config = require('./config');
 const port = process.env.PORT || config.PORT;
 
-const logger = require('./Services/Logger');
-const mysqldb = require('./Services/MySQLService');
-const mongodb = require('./Services/MongoService');
+const Logger = require(__dirname + '/lib/Logger');
+const mysqldb = require(__dirname + '/lib/MySQLService');
+const mongodb = require(__dirname + '/lib/MongoService');
+
 app.use(function (req, res, next) {
-	logger.access(req);
+	Logger.access(req);
 	next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // middlewares
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/public/views'));
+app.set('views', __dirname + '/public/views');
 app.use(express.static(__dirname + '/public'));
 app.use('/static', express.static(__dirname + '/public/static'));
 
-const { IndexRouter } = require('./controller/IndexController');
-const { UtilRouter } = require('./controller/UtilController');
-const { GameRouter } = require('./controller/GameController');
-const { JobRouter } = require('./controller/JobController');
-
-app.use('', IndexRouter);
-app.use('/util', UtilRouter);
-app.use('/game', GameRouter);
-app.use('/job', JobRouter);
+// Includes Router
+app.use('', require('./Controller/IndexController'));
+app.use('/util', require('./Controller/UtilController'));
+app.use('/game', require('./Controller/GameController'));
+app.use('/job', require('./Controller/JobController'));
+app.use('/admin', require('./Controller/AdminController'));
 
 const masterThreadOnStart = function () {
-	logger.info(`Master thread [${process.pid}] start`);
+	Logger.info(`Master thread [${process.pid}] start`);
 	for (let i = 0; i < numCPUs; i++) {
 		cluster.fork();
 	}
 	cluster.on('exit', (worker, code, signal) => {
-		logger.info(`worker ${worker.process.pid} died`);
+		Logger.info(`worker ${worker.process.pid} died`);
 	});
 }
 
@@ -62,18 +60,18 @@ const serverOnCreate = function () {
 	});
 }
 const serverOnStart = function () {
-	logger.info(`Thread [${process.pid}] start`);
-	logger.info(`Server Info: `);
-	logger.info(`Start Time \t: ${Date()}`);
-	logger.info(`listening on \t: ${port}`);
+	Logger.info(`Thread [${process.pid}] start`);
+	Logger.info(`Server Info: `);
+	Logger.info(`Start Time \t: ${Date()}`);
+	Logger.info(`listening on \t: ${port}`);
 }
 
-const serverOnRestart = function(){
+const serverOnRestart = function () {
 
 }
 
 const serverOnDestory = function (err = null) {
-	logger.error(err);
+	Logger.error(err);
 	process.exit(1);
 }
 
