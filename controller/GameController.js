e591cdd45de9c5bcb72b45d5d98427a257abb168
samho <samho@ugcv.com>
2018-05-23 14:30:11 +0800

    readme update

--- controller/GameController.js
+++ controller/GameController.js
@@ -6,7 +6,6 @@ const Logger = require(__dirname + '/../lib/Logger');
 
 var _router = express.Router();
 
-
 _router.get('/no2g', function (req, res) {
     let model = getmodel('www', ['game/no2g.js']);
     _render(res, 'game/no2g.ejs', model);
